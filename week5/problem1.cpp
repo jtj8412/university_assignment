67746ddf30980726e15b9c7c5a7ee1e0e61261cd
jtj8412 <jtj8412@gmail.com>
2021-09-30 21:42:23 +0900

    week5

--- week5/problem1.cpp
+++ week5/problem1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int dx[] = {0, -1, 1, 0}, dy[] = {-1, 0, 0, 1};
+    int dx[] = {-1, 0, 0, 1}, dy[] = {0, -1, 1, 0};
     int n, i, nx, ny, x = 1, y = 1, idx;
     string str;
 
--- week5/problem1.java
+++ week5/problem1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class problem1{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int dx[] = {0, -1, 1, 0}, dy[] = {-1, 0, 0, 1};
+        int dx[] = {-1, 0, 0, 1}, dy[] = {0, -1, 1, 0};
         int n = scanner.nextInt(), i, nx, ny, x = 1, y = 1, idx;
         scanner.nextLine();
         String s = scanner.nextLine();
